92cf70c63df65a7d4f9087e9dd699dfd1a0469ea
icode100 <421150@student.nitandhra.ac.in>
2023-02-18 17:55:18 +0530

    saving

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,10 @@
 import "@/styles/globals.css";
 import Layout from "../components/layout/Layout";
-
+import Landing from "../components/landing/Landing"
 export default function App({ Component, pageProps }) {
   return (
     <Layout>
+      <Landing/>
       <Component {...pageProps} />
     </Layout>
   );
