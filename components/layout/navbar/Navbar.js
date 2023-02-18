6ab42ac3b0c983a3444c748d0650913069d20316
Mano <mano.26q@gmail.com>
2023-02-18 17:51:48 +0530

    updated global css

--- components/layout/navbar/Navbar.js
+++ components/layout/navbar/Navbar.js
@@ -1,5 +1,20 @@
+import { useState } from "react";
+import styles from "./Navbar.module.scss";
+
 function Navbar() {
-  return <div>Navbar</div>;
+  const [active, setActive] = useState(false);
+
+  return (
+    <nav>
+      <div id={styles.hamburger}>
+        <div />
+        <div />
+        <div />
+      </div>
+
+      {active ? <h1>Inactive</h1> : <h1>Active</h1>}
+    </nav>
+  );
 }
 
 export default Navbar;
