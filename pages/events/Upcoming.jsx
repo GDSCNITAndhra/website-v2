72cefcee4766568effb473e15dbad4282e4515cd
icode100 <421150@student.nitandhra.ac.in>
2023-02-19 20:42:22 +0530

    events page done

--- pages/events/Upcoming.jsx
+++ pages/events/Upcoming.jsx
@@ -4,7 +4,7 @@ import styles from"./upcoming.module.css"
 export default function Upcoming() {
   return (
     <div id={styles.upcoming}>
-        
+        <h5>No upcoming Events</h5>
     </div>
   )
 }
