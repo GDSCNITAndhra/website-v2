7aa9ef2f8ef197071f77037704edd5caaa83a60d
Mano <mano.26q@gmail.com>
2023-02-19 11:22:58 +0530

    updated navbar ui

--- components/homepage/HomePage.js
+++ components/homepage/HomePage.js
@@ -2,7 +2,7 @@ import Contact from "../contact/Contact";
 import styles from "./HomePage.module.scss";
 
 function HomePage() {
-  return <div id={styles.homePage}></div>;
+  return <div id={styles.homePage}>{/* <Contact></Contact> */}</div>;
 }
 
 export default HomePage;
--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,13 @@
 import Layout from "../components/layout/Layout";
-import Landing from "../components/landing/Landing";
-import Parallax from "../components/parallax/Parallax";
-import { useState } from "react";
+// import Landing from "../components/landing/Landing";
+// import Parallax from "../components/parallax/Parallax";
+// import { useState } from "react";
+import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
-      <Landing
-        src="https://gdsc-yonsei.github.io/assets/images/GDSC-Logo.png"
-        opacity={1}
-      />
-      <Parallax />
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
   );
 }
