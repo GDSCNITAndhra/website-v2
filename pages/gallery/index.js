2072200ef84270592b2292500b94ce483b82b18b
Mano <mano.26q@gmail.com>
2023-02-23 14:34:39 +0530

    added gallery Images

--- pages/gallery/index.js
+++ pages/gallery/index.js
@@ -10,8 +10,16 @@ import content from "../../content/content.json";
 import "swiper/css";
 import "swiper/css/navigation";
 SwiperCore.use([Navigation]);
-import a from "../../images/android.gif";
-
+import i1 from "../../images/gallery/Screenshot (1054).png";
+import i2 from "../../images/gallery/Screenshot (1055).png";
+import i3 from "../../images/gallery/Screenshot (1059).png";
+import i4 from "../../images/gallery/Screenshot (1061).png";
+import i5 from "../../images/gallery/Screenshot (1063).png";
+import i6 from "../../images/gallery/Screenshot (1065).png";
+import i7 from "../../images/gallery/Screenshot (1066).png";
+import i8 from "../../images/gallery/Screenshot (1067).png";
+import i9 from "../../images/gallery/Screenshot (1069).png";
+const images = [i7, i8, i9, i5, i4, i2, i3, i6, i1];
 const boxVariant = {
   visible: { opacity: 1, scale: 1, transition: { duration: 0.5 } },
   hidden: { opacity: 0, scale: 0 },
@@ -38,18 +46,13 @@ function Index() {
         thumbs={{ swiper: activeThumb }}
         id={styles.gallerySwiper}
       >
-        <SwiperSlide>
-          <Image alt="" src={demo} />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Image alt="" src={demo} />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Image alt="" src={demo} />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Image alt="" src={a} />
-        </SwiperSlide>
+        {images.map((elem, index) => {
+          return (
+            <SwiperSlide key={index}>
+              <Image alt="" src={elem} />
+            </SwiperSlide>
+          );
+        })}
       </Swiper>
     </div>
   );
